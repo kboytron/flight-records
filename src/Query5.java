d64ee7c5ba86741f2e9de5e3147178099a64a7f3
Karan SIngh <ksingh7@uiowa.edu>
2022-05-03 22:29:25 -0400

    q5

--- src/Query5.java
+++ src/Query5.java
@@ -1,10 +1,31 @@
 import java.io.IOException;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Query5 {
     public static String Query5(Iterable<FlightRecord> input) {
-        return null;
+        Map<Integer, Integer> month = new HashMap<>();
+
+        for(FlightRecord cid : input){
+            month.merge(cid.MONTH, 1, Integer::sum);
+            //System.out.println(month.entrySet() + "=" + month.keySet());
+        }
+        Map.Entry<Integer, Integer> max = null;
+        for(Map.Entry<Integer, Integer> entry : month.entrySet()){
+            if(max == null || max.getValue() < entry.getValue()){
+                max = entry;
+            }
+
+        }
+        String mans = null;
+        if (max != null) {
+            mans = max.getKey().toString();
+        }
+        String ans = null;
+        if (max != null) {
+            ans = max.getValue().toString();
+        }
+
+        return mans + " had " + ans + " flights";
     }
 
     public static void main(String[] args) throws IOException {
